91e4ef4710e3bca9b2f589e074e9c2b48717af7a
evan <dklworkdev@gmail.com>
2025-01-22 15:04:59 +0900

    최초 Flask 서버 생성 및 Swagger 설정

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # PythonEdu2
+
+Flask와 RestX를 사용하여 api 서버를 개발합니다.
+
+```
+// 파이썬 라이브러리 설치 방법
+pip install <lib-name>
+
+// 종속성을 관리하기 위한 txt 파일 생성
+pip freeze > requirements.txt
+
+// requirement.txt파일로 관련된 종속성들을 설치
+pip install -r requirements.txt
+
+// Zappa 설치
+pip install zappa
+
+// Zappa 설정
+zappa_settings.json 파일 생성 후 정보 기재
+
+// Zappa 배포
+zappa deploy dev
+```
---
+++ api/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restx import Api
+
+def create_app():
+    app = Flask(__name__)
+
+    # for zappa health check
+    app.add_url_rule('/', endpoint='ping', view_func=lambda: 'Pong!')
+
+    # for swagger
+    authorizations = {
+        'user_token': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization',
+            'description': 'JWT for user'
+        },
+    }
+    api = Api(app,
+              authorizations=authorizations,
+              security='user_token',
+              doc='/swagger',
+              title='Evan-flask-edu',
+              version='1.0',
+              description='Flask-Restx를 이용한 백엔드 API')
+
+    return app
